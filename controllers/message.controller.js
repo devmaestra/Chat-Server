e18b7226d183cde1e66eec2ebfed91a41a1d0749
devmaestra <127052615+devmaestra@users.noreply.github.com>
2023-07-24 19:50:22 -0500

    #U5_02 User

---
+++ controllers/message.controller.js
@@ -0,0 +1,2 @@
+const router = require('express').Router();
+const Messages = require('../models/message.model');
--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const Users = require('../models/users.model');
+const Users = require('../models/user.model');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
 const SECRET = process.env.JWT
